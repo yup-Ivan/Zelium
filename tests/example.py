8a7ed5e28c8655eb3457f9d4d43d63047230c4e5
yup-Ivan <158217864+yup-Ivan@users.noreply.github.com>
2026-02-03 17:30:29 +0100

    Update example.py

--- tests/example.py
+++ tests/example.py
@@ -2,11 +2,7 @@ import zelium
 import time
 
 driver = zelium.start()
-
 zelium.open("https://example.com/", driver)
-
 zelium.click("/html/body/div/p[2]/a", driver)
-
 time.sleep(5)
-
 driver.quit()
